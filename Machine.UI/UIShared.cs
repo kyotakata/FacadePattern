8915ddefdafb941b2003ad5e7ab57ed6d424c3c0
kyotakata <57854222+kyotakata@users.noreply.github.com>
2023-05-27 20:58:54 +0900

    プロジェクト ファイルを追加します。

---
+++ Machine.UI/UIShared.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Machine.UI
+{
+    internal static class UIShared
+    {
+        static UIShared()
+        {
+            if (1 == 1)// 適当な条件文にしている。実際は設定ファイルの値等で判断します。
+            {
+                MachineFacade = new DummyMachineFacade();
+
+            }
+            MachineFacade = new MachineFacade();
+        }
+        internal static IMachineFacede MachineFacade { get; }
+    }
+}
